90219f87984095aca20a0d68da6b1e020c12d47b
Samuel Giacomelli Bruing <sgbruing@gmail.com>
2022-02-08 22:12:28 -0300

    Fisrt implementation operations class

---
+++ app/src/main/java/br/com/letscode/operations.java
@@ -0,0 +1,18 @@
+package br.com.letscode;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import lombok.ToString;
+import lombok.NoArgsConstructor;
+
+@ToString
+@NoArgsConstructor
+
+public class operations {
+
+}
